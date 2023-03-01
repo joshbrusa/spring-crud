1cdc177884d313ff618a47b5c05e28509f4d0840
joshbrusa <joshbrusa@gmail.com>
2023-02-28 21:15:53 -0800

    main

---
+++ src/test/java/com/joshbrusa/posts/PostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joshbrusa.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
